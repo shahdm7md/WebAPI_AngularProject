e7a9ffbfcea3b4b1c4ba3aa0ce7f343cd2d7bf50
Your Name <Your Emailasmaa-salah-ibrahim>
2026-04-13 00:05:09 +0200

    Fix: Add all missing controllers and structures

--- Core/Interfaces/ICategoryRepository.cs
+++ Core/Interfaces/ICategoryRepository.cs
@@ -5,6 +5,8 @@ namespace Core.Interfaces;
 public interface ICategoryRepository
 {
     Task<IEnumerable<Category>> GetAllAsync();
+
+
     Task AddAsync(Category category);
     Task<bool> SaveChangesAsync();
 }
